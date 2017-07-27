44a63d0aefddf9c7e1c61fa03046e5244d045101
Luan Sangguk Lee <sangguk258@gmail.com>
2017-07-27 22:20:42 +0900

    yt works in mac with 'Google Chrome 2'

--- scripts/ytb.sh
+++ scripts/ytb.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-
-open -a "Google Chrome" "https://www.youtube.com/results?search_query=$1"
+cr1='/Applications/Google Chrome.app'
+cr2='/Applications/Google Chrome 2.app'
+if [ -e "$cr2" ]
+then
+  cr=$cr2
+else
+  cr=$cr1
+fi
+open -a "$cr" "https://www.youtube.com/results?search_query=$1"
 exit 0
