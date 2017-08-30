3e4fa4869713ea2e97a3f3098ef5890c52a47cc4
이상국 <luan@ridi.com>
2017-08-30 17:35:07 +0900

    Fix launching Google Chrome 2

--- scripts/lib/chrome.py
+++ scripts/lib/chrome.py
@@ -3,6 +3,6 @@ import os
 
 def launch(url):
     cr1 = '/Applications/Google\ Chrome.app'
-    cr2 = '/Applications/Google\ Chrome 2.app'
-    cr = cr2 if os.path.exists(cr2) else cr1
+    cr2 = '/Applications/Google\ Chrome\ 2.app'
+    cr = cr2 if os.path.exists('/Applications/Google Chrome 2.app') else cr1
     os.system('open -a ' + cr + ' "' + url + '"')
