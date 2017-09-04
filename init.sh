de5e1bbdd89c8e8625eeec21e7250e9fdede40a3
Luan Sangguk Lee <sangguk258@gmail.com>
2017-09-04 23:53:31 +0900

    Rename project

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-AWESOME_SCRIPTS="$HOME/.awesome-scripts"
+OMNIBASH="$HOME/.omnibash"
 BASH_PROFILE="$HOME/.bash_profile"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-rm $AWESOME_SCRIPTS
-echo "source $""HOME/.awesome-scripts" >> $BASH_PROFILE
-echo "export SCRIPTS_PATH='$DIR/'" >> $AWESOME_SCRIPTS
-echo "source $""SCRIPTS_PATH'activate.sh';" >> $AWESOME_SCRIPTS
+rm $OMNIBASH
+echo "source $""HOME/.omnibash" >> $BASH_PROFILE
+echo "export SCRIPTS_PATH='$DIR/'" >> $OMNIBASH
+echo "source $""SCRIPTS_PATH'activate.sh';" >> $OMNIBASH
 
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash\
     -o $SCRIPTS_PATH/git/.git-completion.sh
