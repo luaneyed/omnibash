411df0156a7874c3e24a92bf15b3000ffdf11d4d
Luan Sangguk Lee <sangguk258@gmail.com>
2017-09-06 00:35:58 +0900

    Create cdpl script

--- activate.sh
+++ activate.sh
@@ -1,5 +1,6 @@
 alias c='clear'
 alias cl='. '$OMNIBASH_PATH'/scripts/cl.sh'
+alias cdpl='. '$OMNIBASH_PATH'/scripts/cdpl.sh'
 
 alias cde='. '$OMNIBASH_PATH'/scripts/cde.sh'
 alias yt='. '$OMNIBASH_PATH'/scripts/ytb.sh'
