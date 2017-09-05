3f0e616319a9c4c6fc4238f9827d1d709e5906d3
Luan Sangguk Lee <sangguk258@gmail.com>
2017-09-05 22:48:30 +0900

    Fix initializing

--- init.sh
+++ init.sh
@@ -2,14 +2,14 @@
 
 OMNIBASH="$HOME/.omnibash"
 BASH_PROFILE="$HOME/.bash_profile"
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
 
 rm $OMNIBASH
 echo "source $""HOME/.omnibash" >> $BASH_PROFILE
-echo "export SCRIPTS_PATH='$DIR/'" >> $OMNIBASH
+echo "export SCRIPTS_PATH='$DIR'" >> $OMNIBASH
 echo "source $""SCRIPTS_PATH'activate.sh';" >> $OMNIBASH
 
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash\
-    -o $SCRIPTS_PATH/git/.git-completion.sh
+    -o $DIR'git/.git-completion.sh'
 
 source $BASH_PROFILE
