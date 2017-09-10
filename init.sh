b14c4294dad468d7ebec1e9353e6be79b0761c56
Luan Sangguk Lee <sangguk258@gmail.com>
2017-09-10 23:00:20 +0900

    Fix commit hash of git completion file

--- init.sh
+++ init.sh
@@ -9,7 +9,12 @@ echo "source $""HOME/.omnibash" >> $BASH_PROFILE
 echo "export OMNIBASH_PATH='$DIR'" >> $OMNIBASH
 echo "source $""OMNIBASH_PATH'/activate.sh';" >> $OMNIBASH
 
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash\
-    -o $DIR'/git/.git-completion.sh'
+install_git_completion ()
+{
+    local git_commit_hash='eb37527ab0b6e9ff1cbd01aa20a4dd6aa4150a96'
+    curl https://raw.githubusercontent.com/git/git/$git_commit_hash/contrib/completion/git-completion.bash\
+        -o $DIR'/git/.git-completion.sh'
+}
+install_git_completion
 
 source $BASH_PROFILE
