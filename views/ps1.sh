988ab0e051dba4d9ffe8b9646eddbb8ca3d72077
Luan Sangguk Lee <sangguk258@gmail.com>
2017-09-13 23:20:57 +0900

    Fix overlapping first line

--- views/ps1.sh
+++ views/ps1.sh
@@ -5,36 +5,21 @@ __is_status_clean ()
     echo `git status` | grep "nothing to commit" > /dev/null 2>&1;
 }
 
-__set_user_color ()
-{
-    printf "\033[38;5;45m"
-}
-
-__set_path_color ()
-{
-    printf "\033[38;5;87m"
-}
-
-__set_branch_color ()
-{
-    __is_status_clean
-    if [ $? -eq 0 ]
-    then    #   dirty branch color
-        printf "\033[0;92m"
-    else    #   clean branch color
-        printf "\033[38;5;228m"
-    fi
-}
-
-__set_dollar_color ()
-{
-    printf "\033[38;5;40m"
-}
-
-__reset_color ()
-{
-    printf "\033[0m"
-}
+with_user_color="\[\033[38;5;45m\]"
+with_path_color="\[\033[38;5;87m\]"
+with_branch_color='$(\
+    __is_status_clean;\
+    if [ $? -eq 0 ];\
+    then\
+        #   dirty branch color
+        echo "\[\033[0;92m\]";\
+    else\
+        #   clean branch color
+        echo "\[\033[38;5;228m\]";\
+    fi;\
+)'
+with_dollar_color="\[\033[38;5;40m\]"
+with_default_color="\[\033[0m\]"
 
 __is_git_directory ()
 {
@@ -49,21 +34,19 @@ __print_current_branch ()
     fi
 }
 
-__print_branch_if_git_directory ()
-{
-    __is_git_directory
-    if [ $? -eq 0 ]
-    then
-        echo "$(__set_branch_color)$(__print_current_branch)"
-    fi
-}
 
-print_user='$(__set_user_color)\u'
-print_colon='$(__reset_color): '
-print_path='$(__set_path_color)\w'
-print_branch_if_git_directory='$(__print_branch_if_git_directory)'
-print_dollar='$(__set_dollar_color) \$ '
-reset_color='$(__reset_color)'
+print_user=$with_user_color'\u'
+print_colon=$with_default_color': '
+print_path=$with_path_color'\w'
+print_branch_if_git_directory='$(\
+    __is_git_directory;\
+    if [ $? -eq 0 ];\
+    then\
+        echo "'$with_branch_color'"$(__print_current_branch);\
+    fi\
+)'
+print_dollar=$with_dollar_color' \$ '
+reset_color=$with_default_color
 
 export PS1=\
 $print_user\
