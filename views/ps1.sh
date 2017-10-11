010ba2c88f845f143bb0a34a9c310ed6c06521e6
Luan Sangguk Lee <sangguk258@gmail.com>
2017-10-11 23:56:39 +0900

    Hide variables in ps1.sh from user

--- views/ps1.sh
+++ views/ps1.sh
@@ -1,63 +1,67 @@
 #!/bin/bash
 
-is_status_clean="echo ""\`git status\`"" | grep \"nothing to commit\" > /dev/null 2>&1;";
+#   private function
+#   for limiting scope of variables
+__echo_ps1 ()
+{
+    is_status_clean="echo ""\`git status\`"" | grep \"nothing to commit\" > /dev/null 2>&1;";
 
-set_default_color="\[\033[0m\]";
+    set_default_color="\[\033[0m\]";
 
-set_user_color="\[\033[38;5;45m\]"
+    set_user_color="\[\033[38;5;45m\]"
 
-set_path_color="\[\033[38;5;87m\]";
+    set_path_color="\[\033[38;5;87m\]";
 
-set_dirty_branch_color="\[\033[38;5;228m\]";
+    set_dirty_branch_color="\[\033[38;5;228m\]";
 
-set_clean_branch_color="\[\033[0;92m\]";
+    set_clean_branch_color="\[\033[0;92m\]";
 
-with_branch_color='$(\
-    '$is_status_clean'\
-    if [ $? -eq 0 ];\
-    then\
-        echo "'$set_clean_branch_color'";\
-    else\
-        echo "'$set_dirty_branch_color'";\
-    fi\
-)'
+    with_branch_color='$(\
+        '$is_status_clean'\
+        if [ $? -eq 0 ];\
+        then\
+            echo "'$set_clean_branch_color'";\
+        else\
+            echo "'$set_dirty_branch_color'";\
+        fi\
+    )'
 
-set_dollar_color="\[\033[38;5;40m\]";
+    set_dollar_color="\[\033[38;5;40m\]";
 
-is_git_directory="git branch &> /dev/null";
+    is_git_directory="git branch &> /dev/null";
 
-current_branch_name='$(\
-    b=$(git symbolic-ref HEAD 2> /dev/null);\
-    if [ -n "$b" ];\
-    then\
-        echo " ("${b##refs/heads/}")";\
-    fi\
-)'
+    current_branch_name='$(\
+        b=$(git symbolic-ref HEAD 2> /dev/null);\
+        if [ -n "$b" ];\
+        then\
+            echo " ("${b##refs/heads/}")";\
+        fi\
+    )'
 
-print_branch='$(echo '"$with_branch_color"''"$current_branch_name"')'
+    print_branch='$(echo '"$with_branch_color"''"$current_branch_name"')'
 
-print_user=$set_user_color'\u'
+    print_user=$set_user_color'\u'
 
-print_colon=$set_default_color': '
+    print_colon=$set_default_color': '
 
-path=$set_path_color'\w'
+    path=$set_path_color'\w'
 
-print_branch_if_git_directory='$(\
-    '$is_git_directory';\
-    if [ $? -eq 0 ];\
-    then\
-        echo "'$print_branch'";\
-    fi\
-)'
+    print_branch_if_git_directory='$(\
+        '$is_git_directory';\
+        if [ $? -eq 0 ];\
+        then\
+            echo "'$print_branch'";\
+        fi\
+    )'
 
-print_dollar=$set_dollar_color' \$ '
+    print_dollar=$set_dollar_color' \$ '
 
-reset_color=$set_default_color
+    reset_color=$set_default_color
 
 
-original_ps1=$PS1;
+    original_ps1=$PS1;
 
-ps1=\
+    ps1=\
 $print_user\
 $print_colon\
 $path\
@@ -65,14 +69,16 @@ $print_branch_if_git_directory\
 $print_dollar\
 $reset_color;
 
+    echo_ps1_if_user_opened_bash='$(\
+        if [ "$(whoami)" == "'$(whoami)'" ];\
+        then\
+            echo "'$ps1'";\
+        else\
+            echo "'$original_ps1'";\
+        fi;\
+    )'
 
-echo_ps1_if_user_opened_bash='$(\
-    if [ "$(whoami)" == "'$(whoami)'" ];\
-    then\
-        echo "'$ps1'";\
-    else\
-        echo "'$original_ps1'";\
-    fi;\
-)'
+    echo "$echo_ps1_if_user_opened_bash"
+}
 
-export PS1=$echo_ps1_if_user_opened_bash;
+export PS1=$(__echo_ps1);
