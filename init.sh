ad3bc211fa9315447012a84035115fa853e004da
Luan Sangguk Lee <sangguk258@gmail.com>
2017-11-14 23:00:38 +0900

    Include git-completion file

--- init.sh
+++ init.sh
@@ -12,14 +12,6 @@ fi
 echo "export OMNIBASH_PATH='$DIR'" >> $OMNIBASH
 echo "source $""OMNIBASH_PATH'/activate.sh';" >> $OMNIBASH
 
-install_git_completion ()
-{
-    local git_commit_hash='eb37527ab0b6e9ff1cbd01aa20a4dd6aa4150a96'
-    curl https://raw.githubusercontent.com/git/git/$git_commit_hash/contrib/completion/git-completion.bash\
-        -o $DIR'/git/.git-completion.sh'
-}
-install_git_completion
-
 add_source_to_bash_profile ()
 {
     local source_string="source $""HOME/.omnibash"
