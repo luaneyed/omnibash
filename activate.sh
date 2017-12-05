fd7f3778e128f73c41eb81c4bb89cd8b795ee636
Luan Sangguk Lee <sangguk258@gmail.com>
2017-12-05 22:00:52 +0900

    Create pjs script which makes json pretty

--- activate.sh
+++ activate.sh
@@ -4,6 +4,7 @@ alias cdp='cd ..'
 alias cdpl='. '$OMNIBASH_PATH'/scripts/cdpl.sh'
 
 alias cde='. '$OMNIBASH_PATH'/scripts/cde.sh'
+alias pjs='. '$OMNIBASH_PATH'/scripts/pjs.sh'
 alias yt='. '$OMNIBASH_PATH'/scripts/ytb.sh'
 alias wtr='. '$OMNIBASH_PATH'/scripts/weather.sh'
 
