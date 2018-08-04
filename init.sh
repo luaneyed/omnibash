580c635e6ed848e877a0f460839ecc72205c5efa
Luan Sangguk Lee <sangguk258@gmail.com>
2018-08-04 20:53:05 +0900

    Install .vimrc

--- init.sh
+++ init.sh
@@ -27,5 +27,26 @@ add_source_to_bash_profile ()
     fi
 }
 add_source_to_bash_profile
+unset add_source_to_bash_profile
+
+make_vimrc ()
+{
+    local vimrc_path="$HOME/.vimrc"
+
+    if [ ! -f $vimrc_path ]
+    then
+        touch $vimrc_path
+        echo 'set title' >> $vimrc_path
+        echo 'set number' >> $vimrc_path
+        echo 'set hlsearch' >> $vimrc_path
+        echo 'set incsearch' >> $vimrc_path
+        echo 'set cursorline' >> $vimrc_path
+        echo 'set history=1000' >> $vimrc_path
+        echo 'set showmatch' >> $vimrc_path
+        echo 'syntax on' >> $vimrc_path
+    fi
+}
+make_vimrc
+unset make_vimrc
 
 source $BASH_PROFILE
